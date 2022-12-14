61f11b58d2fb21d57c80896858d1ed4a633c40bd
Bog2G <wegweg3eg@abv.bg>
2022-12-14 23:25:55 +0200

    Basic login and NavBar and some error tweaks

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root 'main#home'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
