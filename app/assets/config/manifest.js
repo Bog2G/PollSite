9a5e552fad69995c802616a6be8cb805768aef1c
Melfric <wegweg3eg@abv.bg>
2022-12-25 23:50:34 +0200

    Little tweaks

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,6 @@
 //= link_tree ../images
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
-//= link_tree ../builds
+//= link_tree .
 //= require vanilla_nested
 //= link application.css
